106498400dcf803f50a54b67976402d237c7a1d8
Ross <ross.russell.ca@gmail.com>
2016-08-23 15:26:11 -0700

    swift enum and Xcode tags.

--- enums/ViewController.swift
+++ enums/ViewController.swift
@@ -10,15 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  //use to replace 1/2/3/4/etc
+  //values are arbitrary
+  
+  enum Cars: Int{
+    case SUV = 0
+    case ALFA = 1
+    case SPIDER = 2
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    myLabel.text = ""
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
+  @IBAction func onTap(sender: UIButton) {
+    if sender.tag == Cars.SUV.rawValue{
+      myLabel.text = "LOL SUV"
+    }else if sender.tag == Cars.ALFA.rawValue{
+      myLabel.text = "Weird rear end"
+    }else {
+      myLabel.text = "look at those eyes."
+    }
+  
+  }
+  @IBOutlet weak var myLabel: UILabel!
 
 
 }
